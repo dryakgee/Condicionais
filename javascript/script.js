ec7ea3788d79d1c47de4004a77642f849d517acc
SABRINA DE FREITAS MELO <sabrina.fmelo1@senacsp.edu.br>
2026-09-08 20:32:52 -0300

    Condicionais

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+let nota, resultado;
+
+function Verificar(){
+    nota = Number(document.getElementById("nota").value);
+    resultado = document.getElementById("resultado");
+
+    if(nota <5){
+        resultado.innerHTML = "Reprovado";
+    }
+    else if(nota < 7){
+    resultado.innerHTML = "Recuperação"
+    }
+
+    else{
+        resultado.innerHTML = "Aprovado";
+    }
+}
