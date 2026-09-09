cc581ba86048d24b15cb4aa62e274e570679f5b0
SABRINA DE FREITAS MELO <sabrina.fmelo1@senacsp.edu.br>
2026-09-08 21:52:26 -0300

    Condicionais

--- javascript/script.js
+++ javascript/script.js
@@ -15,3 +15,45 @@ function Verificar(){
         resultado.innerHTML = "Aprovado";
     }
 }
+
+//Subtração 
+
+let num1, num2, resultado2, subtracao;
+
+function Calcular(){
+    num1 = Number(document.getElementById("num1").value);
+    num2 = Number(document.getElementById("num2").value);
+    resultado2 = document.getElementById("resultado2");
+
+    if(num1 < num2){
+        subtracao = num2 - num1;
+    }
+    else{
+        subtracao = num1 - num2;
+    }
+    resultado2.innerHTML = subtracao;
+}
+
+//Média do aluno
+
+let notaA, notaB, notaC, notaD, media, calculomedia, resultado3;
+
+function Media(){
+    notaA = Number(document.getElementById("notaA").value);
+    notaB = Number(document.getElementById("notaA").value);
+    notaC = Number(document.getElementById("notaA").value);
+    notaD = Number(document.getElementById("notaA").value);
+    media = document.getElementById("media");
+    resultado3 = document.getElementById("resultado3");
+
+    calculomedia = (notaA + notaB + notaC + notaD)/4;
+    
+    if(calculomedia >= 5){
+        resultado3.innerHTML = "Aprovado";
+    }
+    else{   
+        resultado3.innerHTML = "Reprovado";
+
+    }
+        media.innerHTML = "Media: " + calculomedia;
+}
